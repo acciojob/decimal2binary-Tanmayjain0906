e0ef01889794cbc97cf53dbb753da0a5871c4831
Tanmay jain <134151751+Tanmayjain0906@users.noreply.github.com>
2023-07-17 17:17:44 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,14 @@
 function decimalToBinary(num) {
-  //Write you code here
-  
+    let ans = 0;
+    let i = 1;
+    while(x>0)
+    {
+        let rem = x%2;
+        ans = ans + (rem * i);
+        i = i*10;
+        x = parseInt(x/2);
+    }
+	return ans;
 }
 
 window.decimalToBinary = decimalToBinary;
