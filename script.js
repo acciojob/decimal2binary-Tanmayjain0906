d883d9cfb7007756b849935b11965514cfb6514c
Tanmay jain <134151751+Tanmayjain0906@users.noreply.github.com>
2023-07-17 17:19:15 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 function decimalToBinary(num) {
     let ans = 0;
     let i = 1;
-    while(x>0)
+    while(num>0)
     {
         let rem = x%2;
         ans = ans + (rem * i);
         i = i*10;
-        x = parseInt(x/2);
+        num = parseInt(x/2);
     }
 	return ans;
 }
